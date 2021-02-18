355e475b139d7177c21aa2c4f443b324f39a51fa
stoffee <chris@roadkill.org>
2021-02-18 15:47:32 -0800

    adds

--- main.tf
+++ main.tf
@@ -1,3 +1,36 @@
+variable "client_secret" {
+}
+
+variable "subscription_id" {
+}
+
+variable "client_id" {
+}
+
+variable "tenant_id" {
+}
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
 module "resource-group" {
   source  = "rink72/resource-group/azurerm"
   version = "1.0.0"
