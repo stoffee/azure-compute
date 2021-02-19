c9a6818ef49e3de7b0e85fc5fd311c64a50510ad
stoffee <chris@roadkill.org>
2021-02-18 16:20:47 -0800

    more update

--- main.tf
+++ main.tf
@@ -1,4 +1,78 @@
-variable "client_secret" {
+provider "azurerm" {
+  features {}
+    subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "${var.prefix}-vm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  admin_ssh_key {
+    username = "adminuser"
+    public_key = file("fake_id_rsa.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+ variable "client_secret" {
 }
 
 variable "subscription_id" {
@@ -8,46 +82,4 @@ variable "client_id" {
 }
 
 variable "tenant_id" {
-}
-
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
-#terraform {
-#  required_providers {
-#    azurerm = {
-#      source  = "hashicorp/azurerm"
-#      version = "=2.46.0"
-#    }
-#  }
-#}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-}
-
-module "resource-group" {
-  source  = "rink72/resource-group/azurerm"
-  version = "1.0.0"
-  location = "westus2"
-  name = "MyName"
-}
-
-module "vnet" {
-  source  = "Azure/vnet/azurerm"
-  version = "2.3.0"
-  resource_group_name = "MyName"
-}
-
-module "compute" {
-  source  = "Azure/compute/azurerm"
-  version = "3.11.0"
-  license_type = "Windows_Server"
-  resource_group_name = "MyName"
-  vnet_subnet_id = module.vnet.vnet_subnets
 }
