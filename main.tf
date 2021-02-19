18bd0744dbe4d5e34f4fa5463521992a18fa5398
stoffee <chris@roadkill.org>
2021-02-18 16:15:01 -0800

    updates

--- main.tf
+++ main.tf
@@ -12,14 +12,14 @@ variable "tenant_id" {
 
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    azurerm = {
+#      source  = "hashicorp/azurerm"
+#      version = "=2.46.0"
+#    }
+#  }
+#}
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
